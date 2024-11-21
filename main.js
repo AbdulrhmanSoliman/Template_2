5715e47a91a4c0c08ef8f02df80ca283d69390de
Abdulrhman Soliman <abdo.soliman156@gmail.com>
2024-11-21 19:44:29 +0200

    updates

--- main.js
+++ main.js
@@ -6,40 +6,81 @@ const searchInput = document.querySelector("nav .links input[type='text']");
 search.onclick = () => searchInput.classList.toggle("active");
 toggle.onclick = () => toggle.classList.toggle("active");
 
-// pictures silder and active bullets
+// ========= pictures silder and active bullets =========
 const landing = document.querySelector(".landing");
 const bullets = document.querySelectorAll(".landing .bullets span");
-let i = 0;
-let interval = setInterval(() => {
-  i++;
-  landing.style.backgroundImage = `url(images/space${i < 5 ? i : (i = 1)}.jpg`;
+let allSpaceImg = document.querySelectorAll(".bg-imgs img");
+let i = 1; // start from the second image (first one is already in the screen)
+let htmlInterval = setInterval(() => {
+  // automatically change images smoothly using setInterval
+  if (i >= allSpaceImg.length) {
+    i = 0; // reset i value to 0 to start again from the first image
+  }
+  allSpaceImg.forEach((img) => {
+    // make sure all images are 0 opacity
+    img.style.opacity = 0;
+  });
+  allSpaceImg[i].style.opacity = 1; // select the second image (i) to begain the loop
+
   bullets.forEach((e) => {
+    // highlight the related bullet to its image
     if (i == e.dataset.pic) {
       e.classList.add("active");
     } else {
       e.classList.remove("active");
     }
-    e.onclick = function () {
-      landing.style.backgroundImage = `url(images/space${e.dataset.pic}.jpg`;
-      bullets.forEach((e) => e.classList.remove("active"));
-      e.classList.add("active");
-      clearInterval(interval);
-    };
   });
+  i++; // move to the next img and increase the value by 1
 }, 2000);
-//change wallpaper in phones
+// ======================================================
+
+// ========= change image when clicking on each bullet =========
+bullets.forEach((b, index) => {
+  b.addEventListener("click", () => {
+    bullets.forEach((b) => {
+      // make sure all bullets not having class active
+      b.classList.remove("active");
+    });
+    clearInterval(htmlInterval); // stop the spining
+    allSpaceImg.forEach((img) => {
+      // make sure all images are 0 opacity
+      img.style.opacity = 0;
+    });
+    allSpaceImg[index].style.opacity = 1; // select the image connected with its bullet
+    b.classList.add("active"); // set the active class
+  });
+});
+// ======================================================
+// ========= change wallpaper in phones =========
 let rightBtn = document.querySelector(".right");
 let leftBtn = document.querySelector(".left");
+let imageNum = 0; // start from initial value
+
 rightBtn.onclick = function () {
-  i++;
-  landing.style.backgroundImage = `url(images/space${i < 5 ? i : (i = 1)}.jpg`;
-  clearInterval(interval);
+  // change image after clicking right btn in mobile phones
+  clearInterval(htmlInterval);
+  if (imageNum < 3) {
+    imageNum++;
+    changeImage(imageNum);
+  }
 };
+
 leftBtn.onclick = function () {
-  i--;
-  landing.style.backgroundImage = `url(images/space${i >= 1 ? i : (i = 4)}.jpg`;
-  clearInterval(interval);
+  // change image after clicking left btn in mobile phones
+  clearInterval(htmlInterval);
+  if (imageNum > 0) {
+    imageNum--;
+    changeImage(imageNum);
+  }
 };
+
+function changeImage(index) {
+  allSpaceImg.forEach((img) => {
+    img.style.opacity = 0;
+  });
+  allSpaceImg[index].style.opacity = 1;
+}
+// ======================================================
 // PORTFLIO or gallery section suffle or filtertion
 let allImg = document.querySelectorAll(".images picture img");
 let allTitles = document.querySelectorAll(".title li");
@@ -74,7 +115,7 @@ window.addEventListener("scroll", () => {
           if (num.textContent == goal) {
             clearInterval(count);
           }
-        }, 500 / goal);
+        }, goal / 200);
       });
     }
     started = true;
